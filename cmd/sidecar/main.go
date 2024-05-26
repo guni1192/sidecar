d41620b5d40fb7f2823916ce832cc9e0c176cbb3
Takashi IIGUNI <takashi.iiguni@guni.dev>
2024-05-26 23:23:35 +0900

    implement healthcheck
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- cmd/sidecar/main.go
+++ cmd/sidecar/main.go
@@ -13,40 +13,68 @@ import (
 
 var (
 	preExec     string
-	healthcheck string
 	mainCmd     string
 	isDebug     bool
+	healthcheck bool
+	timeout     int
+	interval    int
+	retries     int
+	path        string
+	port        int
+
+	rootCmd = &cobra.Command{
+		Use:   "sidecar",
+		Short: "sidecar is onshot multi-process manager",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			level := slog.LevelInfo
+			if isDebug {
+				level = slog.LevelDebug
+			}
+
+			logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: level,
+			}))
+			slog.SetDefault(logger)
+			slog.Debug("start sidecar", slog.String("pre-exec", preExec), slog.String("main", strings.Join(args, " ")))
+
+			var hc *process.HealthCheck
+			if healthcheck {
+				hc = &process.HealthCheck{
+					Type:     process.HealthCheckTypeHTTP,
+					Port:     port,
+					Path:     path,
+					Retries:  retries,
+					Interval: interval,
+					Timeout:  timeout,
+				}
+			}
+
+			preExecArgs := strings.Split(preExec, " ")
+			psMgr := process.ProcessManager{
+				PreExec: process.PreExec{
+					Process: process.Process{
+						Command: preExecArgs,
+					},
+					HealthCheck: hc,
+				},
+				Main: process.Process{Command: args},
+			}
+
+			ctx := context.Background()
+			return psMgr.Run(ctx)
+		},
+	}
 )
-
-var rootCmd = &cobra.Command{
-	Use:   "sidecar",
-	Short: "sidecar is onshot multi-process manager",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		level := slog.LevelInfo
-		if isDebug {
-			level = slog.LevelDebug
-		}
-
-		logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-			Level: level,
-		}))
-		slog.SetDefault(logger)
-		slog.Debug("start sidecar", slog.String("pre-exec", preExec), slog.String("main", strings.Join(args, " ")))
-
-		preExecArgs := strings.Split(preExec, " ")
-		psMgr := process.ProcessManager{
-			PreExec: process.Process{Command: preExecArgs},
-			Main:    process.Process{Command: args},
-		}
-
-		ctx := context.Background()
-		return psMgr.Run(ctx)
-	},
-}
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&preExec, "pre-exec", "", "", "pre-exec command")
 	rootCmd.PersistentFlags().BoolVarP(&isDebug, "debug", "", false, "show debug log")
+	rootCmd.PersistentFlags().BoolVarP(&healthcheck, "healthcheck", "", false, "enable healthcheck")
+	rootCmd.PersistentFlags().IntVarP(&timeout, "timeout", "", 10, "timeout seconds")
+	rootCmd.PersistentFlags().IntVarP(&interval, "interval", "", 1, "interval seconds")
+	rootCmd.PersistentFlags().IntVarP(&retries, "retries", "", 5, "retries")
+	rootCmd.PersistentFlags().StringVarP(&path, "path", "", "/", "path")
+	rootCmd.PersistentFlags().IntVarP(&port, "port", "", 8000, "port")
 }
 
 func main() {
--- pkg/process/process.go
+++ pkg/process/process.go
@@ -4,18 +4,88 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 )
 
 type ProcessManager struct {
-	PreExec Process
+	PreExec PreExec
 	Main    Process
 }
 
 type Process struct {
 	Command []string
+}
+
+type PreExec struct {
+	Process
+	HealthCheck *HealthCheck
+}
+
+type HealthCheckType string
+
+const (
+	HealthCheckTypeHTTP HealthCheckType = "http"
+	HealthCheckTypeTCP  HealthCheckType = "tcp"
+)
+
+type HealthCheck struct {
+	Type     HealthCheckType
+	Port     int
+	Path     string
+	Retries  int
+	Interval int
+	Timeout  int
+}
+
+func waitCheckHealth(ctx context.Context, hc *HealthCheck) error {
+	for i := 0; i < hc.Retries; i++ {
+		switch hc.Type {
+		case HealthCheckTypeHTTP:
+			err := checkHealthHTTP(ctx, hc)
+			if err != nil {
+				slog.Warn("failed to check health", slog.String("type", string(hc.Type)), slog.String("path", hc.Path), slog.Int("retries", i), slog.String("error", err.Error()))
+			} else {
+				return nil
+			}
+		case HealthCheckTypeTCP:
+			err := checkHealthTCP(ctx, hc)
+			if err != nil {
+				slog.Warn("failed to check health", slog.String("type", string(hc.Type)), slog.String("path", hc.Path), slog.Int("retries", i), slog.String("error", err.Error()))
+			} else {
+				return nil
+			}
+		default:
+			return fmt.Errorf("unknown healthcheck type: %s", hc.Type)
+		}
+		time.Sleep(time.Duration(hc.Interval) * time.Second)
+	}
+	return fmt.Errorf("retry count exceeded")
+}
+
+func checkHealthHTTP(ctx context.Context, hc *HealthCheck) error {
+	client := http.Client{}
+	url := fmt.Sprintf("http://localhost:%d%s", hc.Port, hc.Path)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to request: %w", err)
+	}
+	if res.StatusCode >= 300 {
+		return fmt.Errorf("status code is not success: %d", res.StatusCode)
+	}
+	return nil
+}
+
+func checkHealthTCP(_ctx context.Context, hc *HealthCheck) error {
+	// unimplemented
+	return nil
 }
 
 func (pm *ProcessManager) Run(ctx context.Context) error {
@@ -34,6 +104,13 @@ func (pm *ProcessManager) Run(ctx context.Context) error {
 		return fmt.Errorf("failed to start pre-exec command: %w", err)
 	}
 	slog.Debug("pre-exec command started", slog.Int("pid", preExec.Process.Pid))
+
+	if pm.PreExec.HealthCheck != nil {
+		err = waitCheckHealth(ctx, pm.PreExec.HealthCheck)
+		if err != nil {
+			return fmt.Errorf("failed to wait healthcheck: %w", err)
+		}
+	}
 
 	// main
 	mainCmd := exec.CommandContext(ctx, pm.Main.Command[0], pm.Main.Command[1:]...)
